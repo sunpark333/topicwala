aa67b53bea53cc790f1b48425300ec58df415048
sunpark333 <99256042+sunpark333@users.noreply.github.com>
2026-01-20 00:44:33 +0530

    Change SUPER_ADMINS to environment variable
    Updated SUPER_ADMINS to use environment variable.

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN") or "YOUR_BOT_TOKEN_HERE"
 
 # yahan apna Telegram user id daalo (super admin)
-SUPER_ADMINS = {
-    8097401645,  # <-- replace with your user id
-}
+SUPER_ADMINS = os.getenv("SUPER_ADMINS")
 
 # agar unknown topic mile to jis thread me bhejna ho (General)
 DEFAULT_THREAD_ID = 1
